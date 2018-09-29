ea0207b06dba77043acbd563f4d0d77476e10987
mnshaw <mnshaw@andrew.cmu.edu>
2018-09-29 16:13:46 -0700

    Finish welcome screen

--- animation_exp/lib/WelcomeScreen/welcome.dart
+++ animation_exp/lib/WelcomeScreen/welcome.dart
@@ -9,46 +9,51 @@ class Welcome extends StatefulWidget {
   }
 }
 
+const IconData account_circle = IconData(0xe853, fontFamily: 'MaterialIcons');
+
 class _WelcomeState extends State<Welcome> {
   @override
-
-  int _currentIndex = 0;
-  final List<Widget> _children = [
-    CardDemo(),
-    CardDemo(),
-    CardDemo(),
-  ];
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('volunet'),
-      ),
-      body: _children[_currentIndex], // new
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped, // new
-        currentIndex: _currentIndex, // this will be set when a new tab is tapped
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.mail),
-            title: new Text('Messages'),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: Text('Profile')
-          )
-        ],
+      body: new Stack(
+          children: <Widget>[
+            new Container(
+              decoration: new BoxDecoration(
+              image: new DecorationImage(image: new AssetImage("assets/img4.jpg"), fit: BoxFit.cover,),
+              ),
+            ),
+            new Center(
+              child: new Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  new Text('welcome to volunet',
+                    style: new TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+                new Icon(account_circle, size: 72.0),
+                new RaisedButton(
+                  color: Colors.blue,
+                  child: new Text('Log in with LinkedIn'),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                    new MaterialPageRoute(builder: (context) => new CardDemo()));
+                    },
+                ),
+                new RaisedButton(
+                  child: new Text('Create Profile'),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      new MaterialPageRoute(builder: (context) => new CardDemo()));
+                  },
+                )
+                ],
+              ),
+            ),
+          ],
       ),
     );
-  }
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
   }
 }
